1a8736cb11cd6194f4cd51524a6a5f2b03b3580f
Renan Marques <nan.dev01@gmail.com>
2023-12-14 09:23:09 -0300

    Create a error handling for api problems

--- src/main.js
+++ src/main.js
@@ -6,17 +6,29 @@ import './style.css';
 
 document.querySelector('.cep-button').addEventListener('click', searchCep);
 
+const createErrorElement = () => {
+  const errorElement = document.createElement('h3');
+  errorElement.classList.add('error');
+  errorElement.innerText = 'Algum erro ocorreu, recarregue a página e tente novamente';
+  return errorElement;
+};
+
 const createProductList = async () => {
   const productsSection = document.querySelector('.products');
 
-  addLoading();
-  const computers = await fetchProductsList('computador');
-  computers.forEach(({ id, title, thumbnail, price }) => {
-    const productElement = createProductElement({ id, title, thumbnail, price });
-    productsSection.appendChild(productElement);
-  });
-
-  removeLoading();
+  try {
+    addLoading();
+    const computers = await fetchProductsList('computador');
+    computers.forEach(({ id, title, thumbnail, price }) => {
+      const productElement = createProductElement({ id, title, thumbnail, price });
+      productsSection.appendChild(productElement);
+    });
+  } catch (error) {
+    const errorElement = createErrorElement();
+    productsSection.appendChild(errorElement);
+  } finally {
+    removeLoading();
+  }
 };
 
 createProductList();
